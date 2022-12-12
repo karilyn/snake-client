e347de4f376c1dc6d4cd099c1be563876f51adad
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-12 22:06:41 +0000

    played around with the move up command and then ultimately removed it

--- client.js
+++ client.js
@@ -1,20 +1,28 @@
 const net = require("net");
 // establishes connection with game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',  // IP address here,
     port: 50541 // PORT number here,
   });
   conn.on("data", (data) => {
     console.log("Server says: ", data);
+
   });
+
+  //immediately upon connecting, send initials to server
   conn.on("connect", (connect) => {
-    console.log("Successfully connected to game server.")
-  })
+    console.log("Successfully connected to game server.");
+    conn.write("Name: KLK");
+    // conn.write("Move: up");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   return conn;
 };
 
-module.exports = connect
+module.exports = connect;

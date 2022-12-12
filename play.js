bd717200d099290ca90fb1ffd0f8e4e8b4eaacb5
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-12 22:36:48 +0000

    enabled game termination with CTRL+C

--- play.js
+++ play.js
@@ -2,8 +2,27 @@ const net = require("net");
 
 const connect = require("./client")
 
-
-
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+
+  stdin.on("data", handleUserInput);
+  const handleUserInput = function () {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+
+  return stdin;
+};
 

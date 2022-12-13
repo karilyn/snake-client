42e684669b2a3bb2dc53eab9df152df9f5e89333
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-13 21:54:38 +0000

    modified userInput function to use exported move and message keys

--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, messageKeys } = require("./constants");
 
 // Stores the active TCP connection object. In outer-most scope so it can be used by all functions
 let connection;
@@ -22,22 +23,22 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   };
-  if (key === 'h') {
+  if (key === messageKeys[h]) {
     connection.write("Say: you're hisstory");
   };
-  if (key === 'j') {
+  if (key === messageKeys[j]) {
     connection.write("Say: addercadabra");
   };
 

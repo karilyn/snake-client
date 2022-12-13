1629b043e239497e7d33ca49defba26d00d50e59
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-13 00:48:28 +0000

    added connection parameter to setupInput

--- play.js
+++ play.js
@@ -1,9 +1,12 @@
 const net = require("net");
 
+// Get the networking stuff
 const { connect } = require("./client");
+
+// Get the keyboard stuff
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+let connection = connect();
 
-setupInput();
+setupInput(connection);
